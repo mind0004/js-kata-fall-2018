1b3b52850f96ba93af2af1fe0794f97165d5e848
Mindaugas Jukna <mindaugas9494@hotmail.com>
2018-08-29 12:45:50 +0200

    in progress

---
+++ w1d2 - this site uses cookies/java.js
@@ -0,0 +1,8 @@
+document.querySelector(".end").style.visibility = "hidden";
+
+function gtfo() {
+  //document.getElementById("#reklamer").classList.remove("cookies");
+  document.getElementById("#reklamer").addClass(".cookiesmove");
+  //document.querySelector(".start").style.visibility = "hidden";
+  //document.querySelector(".end").style.visibility = "visible";
+}
