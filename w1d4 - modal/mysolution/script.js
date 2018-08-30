b3a0f3b80e0850cb822e0a02981cf8ccc022556e
Mindaugas Jukna <mindaugas9494@hotmail.com>
2018-08-30 19:42:41 +0200

    d4 done

--- w1d4 - modal/mysolution/script.js
+++ w1d4 - modal/mysolution/script.js
@@ -1,12 +1,18 @@
 "use strict";
 
-document.querySelector("#products").addEventListener = ("click", showModal);
+document.querySelector("#products").addEventListener("click", showModal);
 
 function showModal() {
-  document.getElementById("#modal_container").style.display = "block";
   console.log("Show modal");
+
+  document.querySelector(".popup").style.opacity = "1";
+  document.querySelector(".popup").style.transition = "1s";
+  document.querySelector("#luk").addEventListener("click", hideModal);
 }
 
 function hideModal() {
   console.log("Hide modal");
+
+  document.querySelector(".popup").style.opacity = "0";
+  document.querySelector(".popup").style.transition = "1s";
 }
